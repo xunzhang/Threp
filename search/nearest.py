d98fcc041bdecd6cbcddced0455d3ae13b6eb1a9
wuhong <xunzhangthu@gmail.com>
2013-01-16 21:27:05 +0800

    ./search/nearest.py added

--- search/nearest.py
+++ search/nearest.py
@@ -5,9 +5,29 @@
 
 __author__ = ['Hong Wu<xunzhangthu@gmail.com>']
 
-import numpy as np
-from scipy import spatial
+import kdtree
+  
+def find_nearest_k(stree_base, stree, query_point, k = 4):
+  result_lst = []
+  query = [query_point]
+  # query in search tree.
+  dist = 0.5
+  while len(result_indx) < k:
+    result_indx = stree.query_ball_point(query, dist)
+    dist += 0.25
+  result_indx = result[0 : k]
+  # generate result coord pairs.
+  for indx in result_indx:
+    result_lst.append((stree_base.grid_center_lon[indx], stree_base.grid_center_lat[indx]))
+  return result_lst
 
-class nearest(Exception):
- 
+def find_nearest_dist(stree_base, stree, query_point, dist = 0.5):
+    result_lst = []
+    query = [query_point]
+    result_indx = stree.query_ball_point(query, dist)
+    for indx in result_indx:
+      result_lst.append((stree_base.grid_center_lon[indx], stree_base.grid_center_lat[indx]))
+    return result_lst
 
+if __name__ == '__main__':
+  pass   
