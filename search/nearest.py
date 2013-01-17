8d60c7cef9618868da95ec13bc411494eb572ad3
wuhong <xunzhangthu@gmail.com>
2013-01-17 17:15:51 +0800

    fix a bug of result_indx in ./search/nearest.py

--- search/nearest.py
+++ search/nearest.py
@@ -6,28 +6,40 @@
 __author__ = ['Hong Wu<xunzhangthu@gmail.com>']
 
 import kdtree
-  
-def find_nearest_k(stree_base, stree, query_point, k = 4):
-  result_lst = []
-  query = [query_point]
-  # query in search tree.
-  dist = 0.5
-  while len(result_indx) < k:
-    result_indx = stree.query_ball_point(query, dist)
-    dist += 0.25
-  result_indx = result[0 : k]
-  # generate result coord pairs.
-  for indx in result_indx:
-    result_lst.append((stree_base.grid_center_lon[indx], stree_base.grid_center_lat[indx]))
-  return result_lst
 
-def find_nearest_dist(stree_base, stree, query_point, dist = 0.5):
+class Search(Exception):
+
+  def __init__(self, stree_base, stree):
+    self.stree_base = stree_base
+    self.stree = stree
+    self.dist = 0.5
+    self.delta = 0.25
+  
+  def find_nearest_k(self, query_point, k = 4):
     result_lst = []
     query = [query_point]
-    result_indx = stree.query_ball_point(query, dist)
+    # query in search tree.
+    dist = self.dist
+    result_indx = self.stree.query_ball_point(query, dist)
+    while len(result_indx[0]) < k:
+      result_indx = self.stree.query_ball_point(query, dist)
+      dist += self.delta
+    result_indx = result_indx[0][0 : k]
+    # generate result coord pairs.
     for indx in result_indx:
-      result_lst.append((stree_base.grid_center_lon[indx], stree_base.grid_center_lat[indx]))
-    return result_lst
+      result_lst.append((self.stree_base.grid_center_lon[indx], self.stree_base.grid_center_lat[indx]))
+    return result_indx, result_lst
 
+  def find_nearest_dist(self, query_point, dist = 0.5):
+    result_lst = []
+    query = [query_point]
+    result_indx = self.stree.query_ball_point(query, dist)
+    for indx in result_indx:
+      result_lst.append((self.stree_base.grid_center_lon[indx], self.stree_base.grid_center_lat[indx]))
+    return result_indx, result_lst
+  
+  def find_nearest_triangle(self, query_point, threshold):
+    pass
+  
 if __name__ == '__main__':
   pass   
