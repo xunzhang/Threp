a72e251f68725cfcc6e0aed95a86a75f7191d719
wuhong <xunzhangthu@gmail.com>
2013-01-18 15:55:55 +0800

    test sph_dist of idw_solver

--- geometry/distance.py
+++ geometry/distance.py
@@ -14,6 +14,6 @@ def euc_dist(pt1, pt2):
 # cos-1(cos(dst_lat) * cos(src_lat) * (cos(dst_lon) * cos(src_lon) + sin(dst_lon) * sin(dst_lon)) + sin(dst_lat) * sin(src_lat))     
 # see SCRIP1.4 user guide.
 def sph_dist(pt1, pt2):
-  return math.acos(cos(pt2[1]) * cos(pt1[1]) * (cos(pt2[0]) * cos(pt1[0]) + sin(pt2[0]) * sin(pt1[0])) + sin(pt2[1]) * sin(pt1[1]))
+  return math.acos(math.cos(pt2[1]) * math.cos(pt1[1]) * (math.cos(pt2[0]) * math.cos(pt1[0]) + math.sin(pt2[0]) * math.sin(pt1[0])) + math.sin(pt2[1]) * math.sin(pt1[1]))
   
   
