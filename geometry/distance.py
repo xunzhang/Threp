52809d089f485f5ad920a2ec43983889c78bc2e6
wuhong <xunzhangthu@gmail.com>
2013-01-19 19:20:31 +0800

    fix a bug: cos(radius) in distance.py

--- geometry/distance.py
+++ geometry/distance.py
@@ -14,6 +14,8 @@ def euc_dist(pt1, pt2):
 # cos-1(cos(dst_lat) * cos(src_lat) * (cos(dst_lon) * cos(src_lon) + sin(dst_lon) * sin(dst_lon)) + sin(dst_lat) * sin(src_lat))     
 # see SCRIP1.4 user guide.
 def sph_dist(pt1, pt2):
+  val = math.pi / 180
+  pt1 = (pt1[0] * val, pt1[1] * val)
+  pt2 = (pt2[0] * val, pt2[1] * val)
   return math.acos(math.cos(pt2[1]) * math.cos(pt1[1]) * (math.cos(pt2[0]) * math.cos(pt1[0]) + math.sin(pt2[0]) * math.sin(pt1[0])) + math.sin(pt2[1]) * math.sin(pt1[1]))
-  
   
