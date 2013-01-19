51fe55b6c520249f554d698db09b1877e40a6f69
wuhong <xunzhangthu@gmail.com>
2013-01-19 13:12:11 +0800

    finish idw.py with masks in src and dst grids. Find a bug between radius and degrees in netcdf.

--- interp/algs/idw.py
+++ interp/algs/idw.py
@@ -5,6 +5,7 @@
 
 __author__ = ['Hong Wu<xunzhangthu@gmail.com>'] 
 
+import sys
 import threp_import
 from nearest import Search
 from interp import Interp
@@ -18,22 +19,64 @@ class Idw(Interp):
     self.eps = 1.0e-6
     self.nearest_k = k
     self.idw_obj = Search(self.stree_base_obj, self.stree)
-
+  
+  def select_k(self, indx, lst, k):
+    num = 0
+    a = []
+    b = []
+    for i in range(len(indx)):
+      if self.src_grid_imask[indx[i]] == 0:
+        continue
+      a.append(indx[i])
+      b.append(lst[i])
+      num += 1
+      if num == k:
+        break
+    if num < k:
+      print 'Bugs!!!'
+      sys.exit()
+    return a, b
+   
+  # decide if it is land cell
+  def check_all_masks(self, indx):
+    checksum = 0
+    for i in indx:
+      if self.src_grid_imask[i] == 0:
+        checksum += 1
+    if checksum == 4:
+      return True
+    else:
+      return False
+    
+  def find_neighbors(self, dst_point):
+    indx, lst = self.idw_obj.find_nearest_k(dst_point, self.nearest_k * 2)
+    if self.check_all_masks(indx[0:4]):
+      indx = []
+      lst = []
+    else:
+      indx, lst = self.select_k(indx, lst, self.nearest_k)
+    return indx, lst
+    
   def interp(self):
     n = len(self.dst_grid_center_lon)
     # travese each dst pnt
     for i in range(n):
       # ignore the masked point
-      if self.dst_grid_imask == 0:
+      if self.dst_grid_imask[i] == 0:
+        print 'My mask is zero!'
         continue
       dst_point = (self.dst_grid_center_lon[i], self.dst_grid_center_lat[i])
-      neighbor_indx, neighbor_lst = self.idw_obj.find_nearest_k(dst_point, self.nearest_k)
+      #neighbor_indx, neighbor_lst = self.idw_obj.find_nearest_k(dst_point, self.nearest_k)
+      neighbor_indx, neighbor_lst = self.find_neighbors(dst_point)
+      
       idw_solver = Idw_Solver(dst_point, neighbor_lst, self.eps, self.power)
       idw_solver.solve()
       print dst_point
       print neighbor_lst
       print idw_solver.wgt_lst
+      print ''
 
 if __name__ == '__main__':
   test_obj = Idw('../../grid/T42.nc', '../../grid/POP43.nc', 4)
+  #test_obj = Idw('../../grid/POP43.nc', '../../grid/T42.nc', 4)
   test_obj.interp()
