a3f3aa307938491cc22edd12d360806e8be97d51
wuhong <xunzhangthu@gmail.com>
2013-01-25 19:11:55 +0800

    add install script and testes.

---
+++ install.py
@@ -0,0 +1,25 @@
+#! /usr/bin/python
+# Filename: install.py
+
+import os
+import sys
+from config.threp_dir import THREP_DIR
+
+# check THREP_DIR
+if not THREP_DIR:
+  print 'Please modify var THREP_DIR at ./config/threp_dir.py file.'
+  print 'THREP_DIR needs to be set up with the top Threp directory.'
+  exit(1)
+
+# cp import file for modules
+filename = 'threp_dir.py'
+
+command = 'cp ./config/' + filename + ' ./interp/' + filename
+os.system(command)
+command = 'cp ./config/' + filename + ' ./interp/algs' + filename
+os.system(command)
+command = 'cp ./config/' + filename + ' ./search/' + filename
+os.system(command)
+command = 'cp ./config/' + filename + ' ./interp/solver' + filename
+os.system(command)
+
--- solver/threp_dir.py
+++
@@ -1,5 +0,0 @@
-#! /usr/bin/python
-# Filename: threp_dir.py
-# -*- coding: utf-8
-
-THREP_DIR = '/home/wuhong/research/TRemap/Threp'
