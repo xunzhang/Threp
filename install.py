61652cd139c1f806294bcc0229cbeeae4faf6a76
wuhong <xunzhangthu@gmail.com>
2013-01-28 16:25:49 +0800

    Directory structure changed.

--- install.py
+++ install.py
@@ -7,19 +7,19 @@ from config.threp_dir import THREP_DIR
 
 # check THREP_DIR
 if not THREP_DIR:
-  print 'Please modify var THREP_DIR at ./config/threp_dir.py file.'
+  print 'Please modify var THREP_DIR at ./threp/config/threp_dir.py file.'
   print 'THREP_DIR needs to be set up with the top Threp directory.'
   exit(1)
 
 # cp import file for modules
 filename = 'threp_dir.py'
 
-command = 'cp ./config/' + filename + ' ./interp/' + filename
+command = 'cp ./threp/config/' + filename + ' ./interp/' + filename
 os.system(command)
-command = 'cp ./config/' + filename + ' ./interp/algs' + filename
+command = 'cp ./threp/config/' + filename + ' ./interp/algs' + filename
 os.system(command)
-command = 'cp ./config/' + filename + ' ./search/' + filename
+command = 'cp ./threp/config/' + filename + ' ./search/' + filename
 os.system(command)
-command = 'cp ./config/' + filename + ' ./solver' + filename
+command = 'cp ./threp/config/' + filename + ' ./solver' + filename
 os.system(command)
 
